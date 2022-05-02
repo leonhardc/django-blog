12478acfc3814da47654bd10e122bbc5b303497f
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-05-02 18:28:28 -0300

    Adicionados comentários e drscrições dos arquivos admin e models do app comentarios

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Comentario
 
+
+"""
+
+ComentarioAdmin é a classe de configuração dos campos que serão exibidos, clicáveis e editáveis na janela admin do
+app comentarios.
+
+    - list_display: campos que serão exibidos na janela admin dos comentarios.
+    - list_editable: campos que serão editáveis na janela admin dos comentarios.
+    - list_display_links: campos que serão clicáveis na janela admin dos comentarios.
+
+
+"""
+
+
 class ComentarioAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome_comentario', 'email_comentario',
                     'usuario_comentario', 'post_comentario',
@@ -11,6 +25,7 @@ class ComentarioAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'nome_comentario', 'email_comentario')
 
 
+# admin.register() funciona como uma forma de dizer ao django aplicar o que foi pedido eem ComentarioAdmin
 admin.register(Comentario, ComentarioAdmin)
 
 
--- comentarios/models.py
+++ comentarios/models.py
@@ -3,6 +3,26 @@ from posts.models import Post
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+"""
+
+No arquivo models.py de cada app no django está contido a declaração dos metadados do app no banco de dados configurado.
+
+No caso, o app Comentarios está contigo um model como segue a descrição:
+    - id: identificador do comentario, declarado automaticamente pelo django. É um campo inteiro, Chave Primaria e 
+    Não nulo, além de ser autoincrementado.
+    - nome_comentario: O titulo do comentário. Definido como uma palavra de, no máximo, 150 caracteres.
+    - email_comentario: Email do usuário que publicou o comentário. É declarado como um campo de email, não nulo.
+    - post_comentario: O corpo do comentario. É definido como uma ForengKey com relação com Post: defalt: 'se Post for 
+    excluido, seus comentários também serão.'.
+    - usuario_comentario: Usuário que solicitou a publicação do comentário: Este campo é definido como uma ForegnKey com
+    relação com User: defalt: "Se o usuário for excluido, não faça nada com seus posts."
+    - data_comentario: Data de publicação do Post. Definido como um campo dateTimeFild com valor padrão definido na hora
+    da publicação do post.
+    - publicado_comentario: Campo booleano que define se o comentario será publicado ou não. defalt='Não publique.'
+
+"""
+
+
 class Comentario(models.Model):
     nome_comentario = models.CharField(max_length=150)
     email_comentario = models.EmailField()
