d888f8aa59fbb739d181969a3bed1da1211135e8
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-05-02 16:47:20 -0300

    Corrigido pequeno erro no atributo .now

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Post(models.Model):
     titulo_post = models.CharField(max_length=255)
     autor_post = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    data_post = models.DateTimeField(default=timezone.now())
+    data_post = models.DateTimeField(default=timezone.now)
     conteudo_post = models.TextField()
     excerto_post = models.TextField
     categoria_post = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, blank=True, null=True)
