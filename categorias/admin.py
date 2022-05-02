8cc29dc7f2e2486215059b5c65e814390a561ec0
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-05-02 18:27:27 -0300

    Adicionados comentários e descrições dos arquivos admin e models da app categoria

--- categorias/admin.py
+++ categorias/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import Categoria
 
+"""
+CategoriaAdmin é a classe de configuração dos campos que serão exibidos, clicáveis e editáveis na janela admin do
+app categorias.
 
+    - list_display: campos que serão exibidos na janela admin dos comentarios.
+    - list_display_links: campos que serão clicáveis na janela admin dos comentarios.
+
+"""
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome_cat')
     list_display_links = ('id', 'nome_cat')
 
 
+# admin.register() funciona como uma forma de dizer ao django aplicar o que foi pedido eem CategoriaAdmin
 admin.site.register(Categoria, CategoriaAdmin)
--- categorias/models.py
+++ categorias/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+"""
+No arquivo models.py de cada app no django está contido a declaração dos metadados do app no banco de dados configurado.
+
+Nesse arquivo é defindo os metadados ao app categoria. No caso, um unico meetadado, descrito abaixo:
+    - nome_cat: Nome da Categotia de cada post. Este campo é definido como um modulo de texto com tamanho máximo de
+    50 caracteres.
+
+"""
 
 class Categoria(models.Model):
     nome_cat = models.CharField(max_length=50)
